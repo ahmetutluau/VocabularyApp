d497419de337b581986b75c4c49afc3aae91e1f7
Ahmet Utlu <ahmet.utlu@basefy.com>
2025-05-08 14:16:59 +0200

    Initial Commit

---
+++ VocabularyApp/VocabularyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyAppApp.swift
+//  VocabularyApp
+//
+//  Created by Ahmet Utlu on 8.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
